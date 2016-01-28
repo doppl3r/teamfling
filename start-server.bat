7e34aa326bc83a6c9fedc3fc0bec5f1ca8eba9bf
doppl3r <deben3@gmail.com>
2016-01-28 16:41:01 -0700

    added server launching script yay

---
+++ start-server.bat
@@ -0,0 +1,9 @@
+:: change path1 to the MongoDB bin
+:: change path2 to the project database folder
+:: change path3 to the project directory
+
+set path1="C:\Program Files\MongoDB\Server\3.2\bin"
+set path2="%~dp0..\resources\mongo-data\db"
+set path3="%~dp0"
+start cmd /k "cd %path3% & npm start"
+cd %path1% & mongod --dbpath=%path2%
